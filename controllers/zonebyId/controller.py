d416d018139e988fda2b25f9c580cf0603bae975
Remma27 <emrodriguezso@est.utn.ac.cr>
2024-06-13 10:59:39 -0600

    Refactor ZoneByIdController to return proper status codes
    https://github.com/orgs/utn-integrador-III/projects/7/views/1?pane=issue&itemId=66296020

--- controllers/zonebyId/controller.py
+++ controllers/zonebyId/controller.py
@@ -1,7 +1,6 @@
 from flask_restful import Resource
 from utils.server_response import *
 from models.zone.model import ZoneModel
-from controllers.zone.parser import query_parser_save
 import logging
 
 
@@ -12,12 +11,22 @@ class ZoneByIdController(Resource):
         try:
             result = ZoneModel.get_by_id(id)
             if result:
-                # change to string the ObjectId
+                # Change to string the ObjectId
                 result['_id'] = str(result['_id']) if '_id' in result else None
-                return ServerResponse(data=result, message="Zone found", message_code=OK_MSG, status=StatusCode.OK)
+                return ServerResponse(
+                    data=result, 
+                    message="Zone found", 
+                    message_code=OK_MSG, 
+                    status=StatusCode.OK
+                )
             else:
-                return ServerResponse(data={}, message="Zone not found", message_code=NO_DATA, status=StatusCode.NOT_FOUND)
+                return ServerResponse(
+                    data={}, 
+                    message="Zone does not exist", 
+                    message_code=NO_DATA, 
+                    status=StatusCode.OK
+                )
         
         except Exception as ex:
             logging.error(f"Error getting zone by id: {ex}")
-            return ServerResponse(message="Internal server error", message_code=INTERNAL_SERVER_ERROR_MSG, status=StatusCode.INTERNAL_SERVER_ERROR)
+            return ServerResponse(status=StatusCode.INTERNAL_SERVER_ERROR)
