dfcfa1d509449d5362dc7fda620905b1fd8bae1e
Remma27 <emrodriguezso@est.utn.ac.cr>
2024-06-12 19:57:11 -0600

    US-52 GetById EndPoint
    https://github.com/orgs/utn-integrador-III/projects/7/views/1#

--- controllers/zonebyId/controller.py
+++ controllers/zonebyId/controller.py
@@ -1,5 +1,4 @@
 from flask_restful import Resource
-
 from utils.server_response import *
 from models.zone.model import ZoneModel
 from controllers.zone.parser import query_parser_save
@@ -7,21 +6,18 @@ import logging
 
 
 class ZoneByIdController(Resource):
-    route = '/zone/{id}'
+    route = "/zone/<id>"
 
-    """
-    Get all sites
-    """
-    def get(self):        
+    def get(self, id):
         try:
-            zone = ZoneModel.get_by_id()
-
-            data = [c.to_dict() for c in zone]
-            return ServerResponse(data, status=StatusCode.OK)
+            result = ZoneModel.get_by_id(id)
+            if result:
+                # change to string the ObjectId
+                result['_id'] = str(result['_id']) if '_id' in result else None
+                return ServerResponse(data=result, message="Zone found", message_code=OK_MSG, status=StatusCode.OK)
+            else:
+                return ServerResponse(data={}, message="Zone not found", message_code=NO_DATA, status=StatusCode.NOT_FOUND)
+        
         except Exception as ex:
-            logging.exception(ex)
-            return ServerResponse(status=StatusCode.INTERNAL_SERVER_ERROR)
-
-
-
-  
+            logging.error(f"Error getting zone by id: {ex}")
+            return ServerResponse(message="Internal server error", message_code=INTERNAL_SERVER_ERROR_MSG, status=StatusCode.INTERNAL_SERVER_ERROR)
