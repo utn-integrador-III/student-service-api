9ce9f57d1850f09990b0d039fdab3c50a7489fdb
JOSEEAND <115430851+JOSEEAND@users.noreply.github.com>
2024-06-14 18:26:00 -0600

    US-54

--- controllers/zonebyId/controller.py
+++ controllers/zonebyId/controller.py
@@ -12,34 +12,29 @@ class ZoneByIdController(Resource):
     Get all sites
     """
 
-    def get(self):
-
-    route = "/zone/<id>"
-
     def get(self, id):
         try:
             result = ZoneModel.get_by_id(id)
             if result:
                 # Change to string the ObjectId
-                result['_id'] = str(result['_id']) if '_id' in result else None
+                result["_id"] = str(result["_id"]) if "_id" in result else None
                 return ServerResponse(
-                    data=result, 
-                    message="Zone found", 
-                    message_code=OK_MSG, 
-                    status=StatusCode.OK
+                    data=result,
+                    message="Zone found",
+                    message_code=OK_MSG,
+                    status=StatusCode.OK,
                 )
             else:
                 return ServerResponse(
-                    data={}, 
-                    message="Zone does not exist", 
-                    message_code=NO_DATA, 
-                    status=StatusCode.OK
+                    data={},
+                    message="Zone does not exist",
+                    message_code=NO_DATA,
+                    status=StatusCode.OK,
                 )
-        
+
         except Exception as ex:
             logging.error(f"Error getting zone by id: {ex}")
             return ServerResponse(status=StatusCode.INTERNAL_SERVER_ERROR)
-
 
     """
     Delete a zone by ID
@@ -64,4 +59,3 @@ class ZoneByIdController(Resource):
         except Exception as ex:
             logging.exception(ex)
             return ServerResponse(status=StatusCode.INTERNAL_SERVER_ERROR)
-
