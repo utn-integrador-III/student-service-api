cf7a41ed5bca43484f691f65cb97656b7b7641c4
dferreto <diferretomo@est.utn.ac.cr>
2024-06-19 22:14:15 -0600

    Implementación  endpoint para crear un nuevo reporte de objeto perdido

--- controllers/Lost_objects/controller.py
+++ controllers/Lost_objects/controller.py
@@ -1,12 +1,16 @@
 from flask_restful import Resource
+from flask import request
 from utils.server_response import ServerResponse, StatusCode
 from utils.message_codes import *
 from models.lost_objects.model import LostObjectModel
 import logging
 from datetime import datetime
+from controllers.Lost_objects.parser import query_parser_save
+import logging
+import pytz
 
 class LostObjectsController(Resource):
-    route = '/lostObjects'
+    route = '/lostObject'
 
     """
     Get all lost objects
@@ -28,6 +32,49 @@ class LostObjectsController(Resource):
                 obj['claim_date'] = obj['claim_date'].isoformat() if obj.get('claim_date') else None
 
             return ServerResponse(data=lost_objects, status=StatusCode.OK)
+        except Exception as ex:
+            logging.exception(ex)
+            return ServerResponse(status=StatusCode.INTERNAL_SERVER_ERROR)
+
+
+    def post(self):
+        try:
+            # Get data from the request body
+            data = request.get_json()
+
+            # Validate required name fields
+            if not data.get("name"):
+                return ServerResponse(message='Name is required', 
+                                      message_code=LOST_OBJECTS_NAME_REQUIRED, status=StatusCode.BAD_REQUEST)
+
+            # Validate required description fields
+            if not data.get("description"):
+                return ServerResponse(message='Description is required', 
+                                      message_code=LOST_OBJECTS_DESCRIPTION_REQUIRED, status=StatusCode.BAD_REQUEST)
+
+            # Validate if the lost object already exists by name
+            object_exists = LostObjectModel.get_by_name(data.get("name"))
+            if object_exists:
+                return ServerResponse(message='Lost_object already exists', 
+                                      message_code=LOST_OBJECTS_EXIST, status=StatusCode.CONFLICT)
+
+            # Set the status to "Pending"
+            data["status"] = "Pending"
+
+            # Set the attachment path to "/lost Objects"
+            data["attachment_path"] = '/lostObjects'
+
+            # Set the creation date to Costa Rica time
+            data["creation_date"] = datetime.now(pytz.timezone('America/Costa_Rica')).replace(tzinfo=None)
+
+            # Set claim_date to None
+            data["claim_date"] = None
+
+            # Create and save the new lost object
+            lost_object = LostObjectModel.create(data)
+            return ServerResponse(lost_object.to_dict(), message="Lost_object successfully created", 
+                                  message_code=LOST_OBJECTS_SUCCESSFULLY_CREATED, status=StatusCode.CREATED)
+
         except Exception as ex:
             logging.exception(ex)
             return ServerResponse(status=StatusCode.INTERNAL_SERVER_ERROR)
--- models/lost_objects/model.py
+++ models/lost_objects/model.py
@@ -1,7 +1,32 @@
 from pymongo import MongoClient, errors
+from bson import ObjectId
 from models.lost_objects.db_queries import __dbmanager__
+from bson.errors import InvalidId  # Import InvalidId class
+from datetime import datetime
+import logging
+import pytz
+
 
 class LostObjectModel():
+
+    def __init__(self, name=None, description=None, status='Pending', attachment_path='/lostObjects', _id=None, creation_date=None, claim_date=None):
+        self.name = name
+        self.description = description
+        self.status = status
+        self.attachment_path = attachment_path
+        self.creation_date = creation_date if creation_date else datetime.now(pytz.timezone('America/Costa_Rica')).replace(tzinfo=None)
+        self.claim_date = claim_date
+        self._id = _id
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "description": self.description,
+            "status": self.status,
+            "attachment_path": self.attachment_path,
+            "creation_date": self.creation_date,
+            "claim_date": self.claim_date.isoformat() if self.claim_date else None,
+        }
 
     # @classmethod
     def get_all():
@@ -15,3 +40,26 @@ class LostObjectModel():
                 raise Exception(ex)
         
         return info_db
+    
+    @classmethod
+    def get_by_name(cls, name):
+
+        try:
+            # Search for the zone by name in the database
+            result = __dbmanager__.find_one({"name": name})
+            if result:
+                return cls(_id=result.get("_id"), name=result.get("name"))
+            return None
+        except Exception as ex:
+            logging.exception(ex)
+            raise Exception("Failed to get lost object by name: " + str(ex))
+    
+    @classmethod
+    def create(cls, data):
+        try:
+            object = cls(**data)
+            __dbmanager__.create_data(object.to_dict())  # Insert data as a dictionary
+            return object
+        except Exception as ex:
+            logging.exception(ex)
+            raise Exception("Failed to create lost object: " + str(ex))
