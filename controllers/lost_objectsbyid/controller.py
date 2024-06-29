8d6746313d345fdc6bdf08cd9dc2beff81bbaeb2
Ever Barahona <ebarahona@utn.ac.cr>
2024-06-28 18:39:09 -0600

    latest changes lostkobject by id

--- controllers/lost_objectsbyid/controller.py
+++ controllers/lost_objectsbyid/controller.py
@@ -1,35 +1,37 @@
 from flask_restful import Resource
-from flask import request
-from utils.server_response import ServerResponse, StatusCode
-from utils.message_codes import *
+from utils.server_response import *
 from models.lost_objects.model import LostObjectModel
 import logging
-from controllers.Lost_objects.parser import query_parser_save
-import logging
+from bson.errors import InvalidId
 
 
 class LostObjectByIdController(Resource):
+
     routeById = "/lostObject/<string:id>"
 
-    # Get a lost object by id
-    def get(self, id):
+    """
+    Delete a lostObject Report by ID
+    """
+
+    def delete(self, id):
         try:
-            result = LostObjectModel.getById(id)
+            result = LostObjectModel.delete(id)
             if result:
-                result["_id"] = str(result["_id"]) if "_id" in result else None
                 return ServerResponse(
-                    data=result,
-                    message=LOST_OBJECTS_FOUND,
-                    message_code=OK_MSG,
+                    message="Report successfully deleted",
+                    message_code=LOST_OBJECTS_SUCCESSFULLY_DELETED,
                     status=StatusCode.OK,
                 )
             else:
                 return ServerResponse(
                     data={},
-                    message=LOST_OBJECTS_NOT_FOUND,
-                    message_code=NO_DATA,
-                    status=StatusCode.BAD_REQUEST,
+                    message="The report do not exist and cannot be deleted.",
+                    message_codes=NO_DATA,
+                    status=StatusCode.OK,
                 )
         except Exception as ex:
-            logging.error(ex)
+            logging.exception(ex)
             return ServerResponse(status=StatusCode.INTERNAL_SERVER_ERROR)
+        
+   
+    
