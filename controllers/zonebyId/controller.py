0984692a3bc05231ecd59eaf54c48512bfb0f2a5
LilMat69 <semataoe@est.utn.ac.cr>
2024-07-22 12:16:01 -0600

    SCRUM-123 for lostobject and give zone read permissions

--- controllers/zonebyId/controller.py
+++ controllers/zonebyId/controller.py
@@ -14,7 +14,15 @@ class ZoneByIdController(Resource):
     """
     Get all sites
     """
-    def get(self, id):
+    @auth_required(permission='read', with_args=True)
+    def get(self,id, **kwargs):
+        current_user = kwargs.get('current_user', None)
+        if current_user:
+            # Proceed with access to current_user data
+            print(f"Current user: {current_user}")
+        else:
+            # Handle cases where current_user is not provided
+            print("No user data available")
         try:
             result = ZoneModel.get_by_id(id)
             if result:
